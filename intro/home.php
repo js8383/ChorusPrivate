a0a638264731396f6dcb773b0c86eae3e7f5053e
Jason Song <jiachens@andrew.cmu.edu>
2015-07-02 16:33:10 -0400

    prototype

---
+++ intro/home.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+include_once 'dbconnect.php';
+
+if(!isset($_SESSION['user']))
+{
+    header("Location: index.php");
+}
+$res=mysql_query("SELECT * FROM users WHERE user_id=".$_SESSION['user']);
+$userRow=mysql_fetch_array($res);
+?>
+<!DOCTYPE html >
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    
+    <!-- <link rel="stylesheet" href="style.css" type="text/css" /> -->
+
+    <link rel="stylesheet" href="css/home.css">
+
+    <link href='http://fonts.googleapis.com/css?family=Quicksand:400,700' rel='stylesheet' type='text/css' />
+
+    <link href="http://netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.min.css" rel="stylesheet" />
+
+    <script type="text/javascript" src="http://code.jquery.com/jquery-2.0.0.min.js" /></script>
+    <!--[if IE 7]>
+    <link href="http://netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome-ie7.min.css" rel="stylesheet" />
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <h1>Welcome - <?php echo $userRow['email']; ?></h1>        
+
+    <ul class="form">
+        <li><a class="ask" href="../requester.php?task=3"><i class="icon-question"></i>Ask Question</a></li>
+        <li><a class="answer" href="#"><i class="icon-user"></i>Answer Question <!-- <em>5</em> --></a></li>
+        <li><a class="settings" href="#"><i class="icon-cog"></i>App Settings</a></li>
+        <li><a class="logout" href="logout.php?logout"><i class="icon-signout"></i>Logout</a></li>
+    </ul>
+
+
+</body>
+</html>
--- intro/register.php
+++ intro/register.php
@@ -1,39 +1,38 @@
 <?php
 session_start();
-include_once 'dbconnect.php';
-
 if(isset($_SESSION['user'])!="")
 {
 	header("Location: home.php");
 }
+include_once 'dbconnect.php';
 
-if(isset($_POST['btn-login']))
+if(isset($_POST['btn-signup']))
 {
+	$uname = mysql_real_escape_string($_POST['uname']);
 	$email = mysql_real_escape_string($_POST['email']);
-	$upass = mysql_real_escape_string($_POST['pass']);
-	$res=mysql_query("SELECT * FROM users WHERE email='$email'");
-	$row=mysql_fetch_array($res);
+	$upass = md5(mysql_real_escape_string($_POST['pass']));
 	
-	if($row['password']==md5($upass))
+	if(mysql_query("INSERT INTO users(username,email,password) VALUES('$uname','$email','$upass')"))
 	{
-		$_SESSION['user'] = $row['user_id'];
-		header("Location: home.php");
+		?>
+        <script>alert('successfully registered ');</script>
+        <?php
 	}
 	else
 	{
 		?>
-        <script>alert('wrong details');</script>
+        <script>alert('error while registering you...');</script>
         <?php
 	}
-	
 }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Chorus - Login & Registration System</title>
+<title>Login & Registration System</title>
 <link rel="stylesheet" href="style.css" type="text/css" />
+
 </head>
 <body>
 <center>
@@ -41,16 +40,19 @@ if(isset($_POST['btn-login']))
 <form method="post">
 <table align="center" width="30%" border="0">
 <tr>
-<td><input type="text" name="email" placeholder="Your Email" required /></td>
+<td><input type="text" name="uname" placeholder="User Name" required /></td>
+</tr>
+<tr>
+<td><input type="email" name="email" placeholder="Your Email" required /></td>
 </tr>
 <tr>
 <td><input type="password" name="pass" placeholder="Your Password" required /></td>
 </tr>
 <tr>
-<td><button type="submit" name="btn-login">Sign In</button></td>
+<td><button type="submit" name="btn-signup">Sign Me Up</button></td>
 </tr>
 <tr>
-<td><a href="register.php">Sign Up Here</a></td>
+<td><a href="index.php">Sign In Here</a></td>
 </tr>
 </table>
 </form>
